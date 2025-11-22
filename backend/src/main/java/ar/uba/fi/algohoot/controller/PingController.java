c6efa66a75772771c0c89049e2fc22deca13431b
Gonzalo Laos <llaos@fi.uba.ar>
2025-11-22 13:20:11 -0300

    add unit tests and modify the workflows

--- backend/src/main/java/ar/uba/fi/algohoot/controller/PingController.java
+++ backend/src/main/java/ar/uba/fi/algohoot/controller/PingController.java
@@ -1,4 +1,4 @@
-package ar.uba.fi.algohoot.controllers;
+package ar.uba.fi.algohoot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
