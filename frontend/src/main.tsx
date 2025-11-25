d6dfab84f37dcf75c37f55a506ea86e5cff9bf82
gonzalaos <luisgonza250802@gmail.com>
2025-11-25 15:39:47 -0300

    Refactor imports and use createRoot for rendering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClientProvider } from '@tanstack/react-query'; 
-import { appQueryClient } from "./config/app-query-client"
-import App from './App';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
